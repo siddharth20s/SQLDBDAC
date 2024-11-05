80725d537f8af45b8110cff19bf39339cf18217d
SiddharthRajendran <siddharthrajendran582@gmail.com>
2024-11-05 11:56:04 +0530

    Initial commit for folder

---
+++ testdb/dbo/Tables/Employees.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Employees] (
+    [EmployeeID]  INT             IDENTITY (1, 1) NOT NULL,
+    [FirstName]   NVARCHAR (50)   NOT NULL,
+    [LastName]    NVARCHAR (50)   NOT NULL,
+    [DateOfBirth] DATE            NULL,
+    [HireDate]    DATE            DEFAULT (getdate()) NULL,
+    [JobTitle]    NVARCHAR (100)  NULL,
+    [Salary]      DECIMAL (18, 2) NULL,
+    [IsActive]    BIT             DEFAULT ((1)) NULL,
+    PRIMARY KEY CLUSTERED ([EmployeeID] ASC),
+    CHECK ([Salary]>=(0))
+);
+
